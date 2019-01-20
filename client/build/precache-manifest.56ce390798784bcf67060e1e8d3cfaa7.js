9788fee48449b8a282810d64bfd3ac3ba9038ad3
Jacek <jace3k@gmail.com>
2019-01-20 17:57:08 +0100

    add prod build

---
+++ client/build/precache-manifest.56ce390798784bcf67060e1e8d3cfaa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc3ebb150f20e5cc10fc7474303659e4",
+    "url": "/static/media/background.bc3ebb15.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f00e218825a340e8fae2",
+    "url": "/static/js/main.f00e2188.chunk.js"
+  },
+  {
+    "revision": "5c03abd25c2fd7281e39",
+    "url": "/static/js/1.5c03abd2.chunk.js"
+  },
+  {
+    "revision": "f00e218825a340e8fae2",
+    "url": "/static/css/main.ba4899bd.chunk.css"
+  },
+  {
+    "revision": "63d40975314bebce1737ae14653656b4",
+    "url": "/index.html"
+  }
+];
